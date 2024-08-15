3364466bab3ff224a6f7d405affe6de6828047bd
Erik4421 <134088430+Erik4421@users.noreply.github.com>
2024-08-15 03:53:02 +0000

    Criando banco de dados

--- prefeitura/seguranca/views.py
+++ prefeitura/seguranca/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render, redirect
 
 # Create your views here.
 
-def seguranca(request):
+def defesa_civil(request):
     return render(request, "defesa_civil.html", {})
+
+def guarda_municipal(request):
+    return render(request, "guarda_municipal.html", {})
 
 # Defesa Civil
 
@@ -21,3 +24,25 @@ def alagamento(request):
 
 def desmoronamento(request):
     return render(request, "servicos_defesa_civil/desmoronamento.html", {})
+
+# Guarda Municipal
+def secretaria_de_seguranca_urbana_cidadania(request):
+    return render(request, "servicos_guarda_municipal/secretaria_de_seguranca_urbana_cidadania.html", {})
+
+def solicitacao_de_guarnicao(request):
+    return render(request, "servicos_guarda_municipal/solicitacao_de_guarnicao_para_evento.html", {})
+
+def solicitacao_de_rondas(request):
+    return render(request, "servicos_guarda_municipal/solicitacao_de_rondas_da_guarda_municipal.html", {})
+
+def ligue_para_nos(request):
+    return render(request, "servicos_guarda_municipal/ligue_para_nos.html", {})
+
+def teatro_de_fantoches(request):
+    return render(request, "servicos_guarda_municipal/teatro_de_fantoches.html", {})
+
+def ouvidoria(request):
+    return render(request, "servicos_guarda_municipal/ouvidoria.html", {})
+
+def denuncie_aqui(request):
+    return render(request, "servicos_guarda_municipal/denuncie_aqui.html", {})
